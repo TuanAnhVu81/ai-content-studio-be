332730381e5eae10c9afffaa32a0a993deaad233
TuanAnhVu81 <tuananhvu1123@gmail.com>
2026-03-19 07:40:50 +0700

    Initial commit

---
+++ src/main/java/com/portfolio/aicontentstudio/AiContentStudioBeApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.aicontentstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiContentStudioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiContentStudioBeApplication.class, args);
+	}
+
+}
